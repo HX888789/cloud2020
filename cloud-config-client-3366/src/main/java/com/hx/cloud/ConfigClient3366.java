00e1f87252ce721ea1a523e836053692f3b66084
huangxu <1258588259@qq.com>
2021-08-11 18:15:12 +0800

    springcloud seata

--- cloud-config-client-3366/src/main/java/com/hx/cloud/ConfigClient3366.java
+++ cloud-config-client-3366/src/main/java/com/hx/cloud/ConfigClient3366.java
@@ -9,5 +9,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class ConfigClient3366 {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClient3366.class,args);
+
     }
 }
